d8d5e16eedde31d9812e86765a2ec34341d6d010
Nardos Mamo <107912794+Nardos-Mamo@users.noreply.github.com>
2023-12-18 14:32:35 -0500

    Add files via upload

---
+++ ARTime/ARTimePortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARTimePortalApp.swift
+//  ARTimePortal
+//
+//  Created by Nardos Mamo on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ARTimePortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
